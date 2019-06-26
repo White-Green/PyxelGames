309e83c1010df7e0eea5b606abcbd0b297e9ea22
White-Green <>
2019-06-26 16:29:28 +0900

    マンデルブロ集合描画の負荷を軽くした

--- mandel.py
+++ mandel.py
@@ -1,20 +1,31 @@
 import pyxel
+import math
 
 SIZE = 256
+BLOCK_SIZE = 64
 
 
 class Mandel:
     def __init__(self):
         self.r = [0.0] * SIZE * SIZE
         self.i = [0.0] * SIZE * SIZE
+        self.block_index = 0
+        self.block_width = math.ceil(SIZE / BLOCK_SIZE)
+        self.color = 0
         pyxel.init(SIZE, SIZE)
         pyxel.run(self.update, self.draw)
         pyxel.cls(0)
         pyxel.rect(0, 0, 64, 64, 1)
 
     def update(self):
-        for x in range(SIZE):
-            for y in range(SIZE):
+        x_base = (self.block_index // self.block_width) * BLOCK_SIZE
+        y_base = (self.block_index % self.block_width) * BLOCK_SIZE
+        for _x in range(BLOCK_SIZE):
+            for _y in range(BLOCK_SIZE):
+                x = x_base + _x
+                y = y_base + _y
+                if x >= SIZE or y >= SIZE:
+                    break
                 r = self.r[x * SIZE + y]
                 i = self.i[x * SIZE + y]
                 if r ** 2 + i ** 2 > 4:
@@ -24,7 +35,11 @@ class Mandel:
                 nr = self.r[x * SIZE + y] = r * r - i * i + cr
                 ni = self.i[x * SIZE + y] = 2 * r * i + ci
                 if nr ** 2 + ni ** 2 > 4:
-                    pyxel.pix(x, y, (pyxel.frame_count + 1) % 16)
+                    pyxel.pix(x, y, (self.color + 1) % 16)
+        self.block_index += 1
+        if self.block_index >= self.block_width * self.block_width:
+            self.color += 1
+            self.block_index -= (self.block_width * self.block_width)
 
     def draw(self):
         pass
